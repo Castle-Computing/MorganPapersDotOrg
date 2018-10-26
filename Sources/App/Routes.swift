003a5a26fcc57516214d4d7058d91d6b5b32d964
tigeriv <jwilsoniv@me.com>
2018-10-25 18:52:17 -0700

    Add files via upload

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -1,104 +1,51 @@
 import Vapor
 
-/// Register your application's routes here.
+//All routes
 public func routes(_ router: Router) throws {
-    // "It works" page
+    //Home screen, accessible without any additional URL parameters
     router.get { req in
         return try req.view().render("home")
     }
     
-    // Says hello
+    //Adds new route, with search parameter.
+    //Returns a view in the future
     router.get("search") { req -> Future<View> in
+        //Attempt to obtain search term, otherwise abort
+        //Receive the search term from the query
+        //Request host/search?query=
         guard let searchTerm = req.query[String.self, at: "query"] else {
             throw Abort(.badRequest)
         }
         
+        //Replaces all characters of searchTerm not in allowed characters with percent
+        //Encoded characters. .urlQueryAllowed is the character set.
         guard let encodedSearchTerm = searchTerm.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
             throw Abort(.badRequest)
         }
         
-        let searchURL = "https://digital.lib.calpoly.edu/islandora/rest/v1/solr/RELS_EXT_hasModel_uri_t:bookCModel%20AND%20ancestors_ms:%22rekl:morgan-ms010%22%20AND%20(dc.title:" + encodedSearchTerm + "%20OR%20dc.description:" +  encodedSearchTerm + ")?rows=10&omitHeader=true&wt=json"
+        //Create URL for Islandora entries with encoded search title and description
+        //Using SOLR search parameters
+        //Returns maximum 10 documents (rows=10)
+        //Excludes header
+        let searchURL = "https://digital.lib.calpoly.edu/islandora/rest/v1/solr/RELS_EXT_hasModel_uri_t:bookCModel%20AND%20ancestors_ms:%22rekl:morgan-ms010%22%20AND%20(dc.title:" + encodedSearchTerm + "%20OR%20dc.description:" + encodedSearchTerm + ")?rows=10&omitHeader=true&wt=json"
         
+        //Create a client to send a request.get()
         let client = try req.client()
         
+        //Sends an HTTP GET request to URL
+        //The headers are required in the HTTP request, parameter User-Agent has value
+        //MorganApp/0.1
         return client.get(searchURL, headers: HTTPHeaders.init([("User-Agent", "MorganApp/0.1")]))
+            //flatMap unwraps the response and returns a SearchResult in the future
             .flatMap { response -> Future<SearchResult> in
+                //Decode response into a SearchResult
                 try response.content.decode(SearchResult.self)
             }
+            //Take the SearchResult future (result), and try to render it
             .flatMap { result in
-                //debugPrint(result.response.docs)
-                return try req.view().render("results", ResultPageStruct(searchTerm: searchTerm, searchResults: result.response.docs, numResults: result.response.numFound, start: result.response.start))
+                //Render a view for the initial get request
+                //results.leaf, pass a ResultPage
+                return try req.view().render("results", ResultPage(searchTerm: searchTerm, searchResults: result.response.docs, numResults: result.response.numFound, start: result.response.start))
             }
-    }
-    
-    struct ResultPageStruct: Codable {
-        let searchTerm: String
-        let searchResults: [docArray]
-        let numberOfResults: Int
-        let start: Int
-        
-        init(searchTerm: String, searchResults: [docArray], numResults: Int, start: Int) {
-            self.searchTerm = searchTerm
-            self.searchResults = searchResults
-            self.numberOfResults = numResults
-            self.start = start
-        }
-    }
-    
-    struct SearchResult: Codable {
-        let response: searchResponse
-    }
-    
-    struct searchResponse: Codable {
-        let numFound: Int
-        let start: Int
-        let docs: [docArray]
-    }
-    
-    struct docArray: Codable {
-        var titleArray: [String]?
-        var title: String?
-        
-        var authorArray: [String]?
-        var author: String?
-        
-        var descriptionArray: [String]?
-        var description: String?
-        
-        var pid: String
-        
-        enum CodingKeys: String, CodingKey {
-            case titleArray = "dc.title"
-            case title = "title"
-            case authorArray = "dc.contributor"
-            case author = "author"
-            case descriptionArray = "dc.description"
-            case description = "description"
-            case pid = "PID"
-        }
-        
-        init(from decoder: Decoder) throws {
-            let container = try decoder.container(keyedBy: CodingKeys.self)
-            
-            titleArray = try container.decode([String].self, forKey: .titleArray)
-            title = titleArray![0]
-            
-            let reklPid = try container.decode(String.self, forKey: .pid)
-            pid = String(reklPid.split(separator: ":")[1])
-            
-            do {
-                authorArray = try container.decode([String].self, forKey: .authorArray)
-                author = authorArray![0]
-            } catch {
-                debugPrint("No contributor listed.")
-            }
-            
-            do {
-                descriptionArray = try container.decode([String].self, forKey: .descriptionArray)
-                description = descriptionArray![0]
-            } catch {
-                debugPrint("No description listed.")
-            }
-        }
     }
 }
