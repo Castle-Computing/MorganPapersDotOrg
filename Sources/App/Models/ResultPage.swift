33937d282a4156e9ebd4fb686d3937786ed2c3f2
Joey Wilson <jwilsoniv@me.com>
2018-11-28 13:55:03 -0800

    Updates

---
+++ Sources/App/Models/ResultPage.swift
@@ -0,0 +1,111 @@
+//
+//  ResultPage.swift
+//  App
+//
+//  Created by tigeriv on 10/25/18.
+//
+final class ResultPage: Codable {
+    let searchTerm: String
+    let searchResults: [docArray]
+    let numberOfResults: Int
+    let start: Int
+    let page: Int
+    
+    init(searchTerm: String, searchResults: [docArray], numResults: Int, start: Int, page: Int) {
+        self.searchTerm = searchTerm
+        self.searchResults = searchResults
+        self.numberOfResults = numResults
+        self.start = start
+        self.page = page
+    }
+}
+
+struct SearchResult: Codable {
+    let response: searchResponse
+}
+
+struct searchResponse: Codable {
+    let numFound: Int
+    let start: Int
+    let docs: [docArray]
+}
+
+final class docArray: Codable {
+    var titleArray: [String]?
+    var title: String?
+    
+    var authorPlaceholder: String?
+    var author: String?
+    
+    var descriptionArray: [String]?
+    var description: String?
+    
+    var datePlaceholder: String?
+    var date: String?
+    
+    var cityPlaceholder: String?
+    var statePlaceholder: String?
+    var location: String?
+    
+    var pid: String
+    
+    enum CodingKeys: String, CodingKey {
+        case titleArray = "dc.title"
+        case title = "title"
+        case authorPlaceholder = "mods_name_personal_author_namePart_s"
+        case author = "author"
+        case descriptionArray = "dc.description"
+        case description = "description"
+        case pid = "PID"
+        case datePlaceholder = "mods_originInfo_dateCreated_s"
+        case date = "date"
+        case cityPlaceholder = "mods_subject_hierarchicalGeographic_city_s"
+        case statePlaceholder = "mods_subject_hierarchicalGeographic_state_s"
+        case location = "location"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        titleArray = try container.decode([String].self, forKey: .titleArray)
+        title = titleArray![0]
+        
+        pid = try container.decode(String.self, forKey: .pid)
+        
+        do {
+            authorPlaceholder = try container.decode(String.self, forKey: .authorPlaceholder)
+            author = authorPlaceholder
+        } catch {
+            debugPrint("No author listed.")
+        }
+        
+        do {
+            datePlaceholder = try container.decode(String.self, forKey: .datePlaceholder)
+            date = datePlaceholder
+        } catch {
+            debugPrint("No date listed.")
+        }
+        
+        do {
+            cityPlaceholder = try container.decode(String.self, forKey: .cityPlaceholder)
+            statePlaceholder = try container.decode(String.self, forKey: .statePlaceholder)
+        } catch {
+            debugPrint("No location listed.")
+        }
+        
+        if cityPlaceholder?.count ?? 0 > 0 && statePlaceholder?.count ?? 0 > 0 {
+            location = cityPlaceholder! + ", " + statePlaceholder!
+        } else if cityPlaceholder?.count ?? 0 > 0 {
+            location = cityPlaceholder
+        } else if statePlaceholder?.count ?? 0 > 0 {
+            location = statePlaceholder
+        }
+        
+        do {
+            descriptionArray = try container.decode([String].self, forKey: .descriptionArray)
+            description = descriptionArray![0]
+        } catch {
+            debugPrint("No description listed.")
+        }
+    }
+}
--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -1,21 +1,84 @@
 import Vapor
 
-/// Register your application's routes here.
+//All routes
 public func routes(_ router: Router) throws {
-    // "It works" page
+    //Home screen, accessible without any additional URL parameters
     router.get { req in
         return try req.view().render("home")
     }
     
-    // Says hello
+    router.get("letter", String.parameter) { req -> Future<View> in
+        let context = try req.parameters.next(String.self)
+        //Create a client to send a request.get()
+        let client = try req.client()
+        //Sends an HTTP GET request to URL
+        let searchURL = getChildrenURL(pid: context)
+        
+        return client.get(searchURL, headers: HTTPHeaders.init([("User-Agent", "MorganApp/0.1"), ("Authorization", "Basic Y2FzdGxlX2NvbXB1dGluZzo4PnoqPUw0QmU2TWlEP1FB")]))
+            .flatMap { response -> Future<AncestorSearchResult> in
+                return try response.content.decode(AncestorSearchResult.self)
+        }
+            .flatMap { result in
+                var i = 0
+                var pageArray: [PageInfo] = []
+                for pageIn in result.response.docs {
+                    pageArray.append(PageInfo(index1: i, PID1: pageIn.PID))
+                    i = i + 1
+                }
+                return try req.view().render("letter", LetterPage(searchResults: result.response.numFound, searchData: pageArray, parent: context))
+        }
+    }
+
+    router.get("help") { req -> Future<View> in
+        return try req.view().render("about")
+    }
+    
+    //Adds new route, with search parameter.
+    //Returns a view in the future
     router.get("search") { req -> Future<View> in
+        //Attempt to obtain search term, otherwise abort
+        //Receive the search term from the query
+        //Request host/search?query=
         guard let searchTerm = req.query[String.self, at: "query"] else {
             throw Abort(.badRequest)
         }
         
-        return try req.view().render("results", [
-            "searchTerm": searchTerm
-        ])
+        var page = req.query[Int.self, at: "page"] ?? 1
+        if page < 1 {
+            page = 1
+        }
+        
+        let start = (page - 1) * 15
+        
+        //Replaces all characters of searchTerm not in allowed characters with percent
+        //Encoded characters. .urlQueryAllowed is the character set.
+        guard let encodedSearchTerm = searchTerm.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            throw Abort(.badRequest)
+        }
+        
+        //Create URL for Islandora entries with encoded search title and description
+        //Using SOLR search parameters
+        //Returns maximum 10 documents (rows=10)
+        //Excludes header
+        let searchURL = searchBookURL(item: encodedSearchTerm)
+        
+        //Create a client to send a request.get()
+        let client = try req.client()
+        
+        //Sends an HTTP GET request to URL
+        //The headers are required in the HTTP request, parameter User-Agent has value
+        //MorganApp/0.1
+        return client.get(searchURL, headers: HTTPHeaders.init([("User-Agent", "MorganApp/0.1"), ("Authorization", "Basic Y2FzdGxlX2NvbXB1dGluZzo4PnoqPUw0QmU2TWlEP1FB")]))
+            //flatMap unwraps the response and returns a SearchResult in the future
+            .flatMap { response -> Future<SearchResult> in
+                //Decode response into a SearchResult
+                try response.content.decode(SearchResult.self)
+            }
+            //Take the SearchResult future (result), and try to render it
+            .flatMap { result in
+                //Render a view for the initial get request
+                //results.leaf, pass a ResultPage
+                return try req.view().render("results", ResultPage(searchTerm: searchTerm, searchResults: result.response.docs, numResults: result.response.numFound, start: result.response.start, page: page))
+        }
     }
-    
 }
