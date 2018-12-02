06aab15bb4ff3a7d50f192335eac01518c2b5110
Ethan Kusters <ethan@outlook.com>
2018-12-01 17:12:11 -0800

    Integrated letter view page

--- Sources/App/Models/LetterPage.swift
+++ Sources/App/Models/LetterPage.swift
@@ -7,52 +7,14 @@
 
 import Foundation
 
-//For Pages
-/*final class IslandoraObject: Codable {
-    let pid: String
-    let label: String
-    let models: [String]
-    let datastreams: [Datastream]
-}
-
-final class Datastream: Codable {
-    let dsid: String
-    let label: String
-    let size: Int
-}*/
-
-//For Books
-struct AncestorSearchResult: Codable {
-    let response: AncestorResponse
-}
-
-struct AncestorResponse: Codable {
-    let numFound: Int
-    let start: Int
-    let docs: [PageArray]
-}
-
-struct PageArray: Codable {
-    let PID: String
-}
-
 final class LetterPage: Codable {
-    let parentLabel: String
-    let numberResults: Int
-    let data: [PageInfo]
-    init (searchResults: Int, searchData: [PageInfo], parent: String)
+    let letterTitle: String?
+    let children: [String]?
+    let ocrText: String?
+    init (title: String?, children: [String]?, ocrText: String?)
     {
-        self.numberResults = searchResults
-        self.data = searchData
-        self.parentLabel = parent
-    }
-}
-
-final class PageInfo: Codable{
-    let PID: String
-    let index: Int
-    init (index1: Int, PID1: String) {
-        self.index = index1
-        self.PID = PID1
+        self.letterTitle = title
+        self.children = children
+        self.ocrText = ocrText
     }
 }
