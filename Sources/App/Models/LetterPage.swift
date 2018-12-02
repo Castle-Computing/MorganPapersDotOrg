314a4779ddd16b98b7f4c489edada859466ac55a
Joey Wilson <jwilsoniv@me.com>
2018-12-01 19:52:38 -0800

    Incorporated my changes into master

--- Sources/App/Models/LetterPage.swift
+++ Sources/App/Models/LetterPage.swift
@@ -4,17 +4,53 @@
 //
 //  Created by tigeriv on 10/30/18.
 //
-
 import Foundation
 
+//For Pages
+/*final class IslandoraObject: Codable {
+ let pid: String
+ let label: String
+ let models: [String]
+ let datastreams: [Datastream]
+ }
+ final class Datastream: Codable {
+ let dsid: String
+ let label: String
+ let size: Int
+ }*/
+
+//For Books
+struct AncestorSearchResult: Codable {
+    let response: AncestorResponse
+}
+
+struct AncestorResponse: Codable {
+    let numFound: Int
+    let start: Int
+    let docs: [PageArray]
+}
+
+struct PageArray: Codable {
+    let PID: String
+}
+
 final class LetterPage: Codable {
-    let letterTitle: String?
-    let children: [String]?
-    let ocrText: String?
-    init (title: String?, children: [String]?, ocrText: String?)
+    let parentLabel: String
+    let numberResults: Int
+    let data: [PageInfo]
+    init (searchResults: Int, searchData: [PageInfo], parent: String)
     {
-        self.letterTitle = title
-        self.children = children
-        self.ocrText = ocrText
+        self.numberResults = searchResults
+        self.data = searchData
+        self.parentLabel = parent
+    }
+}
+
+final class PageInfo: Codable{
+    let PID: String
+    let index: Int
+    init (index1: Int, PID1: String) {
+        self.index = index1
+        self.PID = PID1
     }
 }
