27e6f3c3d30812ee65a63712919754840cf5111c
samuelverkruyse <samuelverkruyse@gmail.com>
2019-01-17 18:13:11 -0800

    Timeline developments

--- Sources/App/Models/ResultPage.swift
+++ Sources/App/Models/ResultPage.swift
@@ -10,7 +10,7 @@ final class ResultPage: Codable {
     let numberOfResults: Int
     let start: Int
     let page: Int
-    
+
     init(searchTerm: String, searchResults: [docArray], numResults: Int, start: Int, page: Int) {
         self.searchTerm = searchTerm
         self.searchResults = searchResults
@@ -33,25 +33,25 @@ struct searchResponse: Codable {
 final class docArray: Codable {
     var titleArray: [String]?
     var title: String?
-    
+
     var authorPlaceholder: String?
     var author: String?
-    
+
     var descriptionArray: [String]?
     var ocrText: String?
     var description: String?
-    
+
     var datePlaceholder: String?
     var date: String?
-    
+
     var cityPlaceholder: String?
     var statePlaceholder: String?
     var location: String?
-    
+
     var children: [String]?
-    
+
     var pid: String
-    
+
     enum CodingKeys: String, CodingKey {
         case titleArray = "dc.title"
         case title = "title"
@@ -68,47 +68,47 @@ final class docArray: Codable {
         case location = "location"
         case children = "BOOK_CHILDREN_t"
     }
-    
+
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        
+
         titleArray = try container.decode([String].self, forKey: .titleArray)
         title = titleArray![0]
-        
+
         pid = try container.decode(String.self, forKey: .pid)
-        
+
         do {
             authorPlaceholder = try container.decode(String.self, forKey: .authorPlaceholder)
             author = authorPlaceholder
         } catch {
             debugPrint("No author listed.")
         }
-        
+
         do {
             datePlaceholder = try container.decode(String.self, forKey: .datePlaceholder)
             date = datePlaceholder
         } catch {
             debugPrint("No date listed.")
         }
-        
+
         do {
             let childrenString = try container.decode(String.self, forKey: .children)
             children = childrenString.components(separatedBy: ",")
         } catch {
             debugPrint("No children listed.")
         }
-        
+
         do {
             cityPlaceholder = try container.decode(String.self, forKey: .cityPlaceholder)
             statePlaceholder = try container.decode(String.self, forKey: .statePlaceholder)
         } catch {
             debugPrint("No location listed.")
         }
-        
+
         do {
             ocrText = try container.decode(String.self, forKey: .ocrText)
         }
-        
+
         if cityPlaceholder?.count ?? 0 > 0 && statePlaceholder?.count ?? 0 > 0 {
             location = cityPlaceholder! + ", " + statePlaceholder!
         } else if cityPlaceholder?.count ?? 0 > 0 {
@@ -116,7 +116,7 @@ final class docArray: Codable {
         } else if statePlaceholder?.count ?? 0 > 0 {
             location = statePlaceholder
         }
-        
+
         do {
             descriptionArray = try container.decode([String].self, forKey: .descriptionArray)
             if descriptionArray?.count ?? 0 > 0 && descriptionArray![0].count > 0 {
@@ -132,7 +132,7 @@ final class docArray: Codable {
             } else {
                 debugPrint("No description or OCR listed.")
             }
-         
+
         }
     }
 }
--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -3,30 +3,32 @@ import Vapor
 //All routes
 public func routes(_ router: Router) throws {
     //Home screen, accessible without any additional URL parameters
+
     router.get { req in
         return try req.view().render("home")
     }
-    
+
+<<<<<<< Updated upstream
     router.get("letter", String.parameter) { req -> Future<View> in
         let letterPID = try req.parameters.next(String.self)
         let client = try req.client()
         let letterURL = getLetterURL(pid: letterPID)
-        
+
         let searchResultRequest = client.get(letterURL, headers: HTTPHeaders.init([("User-Agent", "MorganApp/0.1")]))
             .flatMap { firstResponse -> Future<SearchResult> in
                 return try firstResponse.content.decode(SearchResult.self)
         }
-        
+
         let ocrDataRequest = client.get("https://digital.lib.calpoly.edu/islandora/object/" + letterPID + "/datastream/OCR_BOOK", headers: HTTPHeaders.init([("User-Agent", "MorganApp/0.1")]))
             .flatMap { secondResponse -> Future<Data> in
                 return secondResponse.http.body.consumeData(on: req)
         }
-        
+
         return flatMap(searchResultRequest, ocrDataRequest) { (result: SearchResult, ocrData: Data) in
                 guard let letter = result.response.docs.first else {
                     throw Abort(.badRequest)
                 }
-            
+
                 let ocrText = String(data: ocrData, encoding: .utf8) ?? "invalid encoding"
                 return try req.view().render("letter", LetterPage(title: letter.title, children: letter.children, ocrText: ocrText, numPages: letter.children?.count, metadata: letter))
         }
@@ -35,15 +37,100 @@ public func routes(_ router: Router) throws {
     router.get("help") { req -> Future<View> in
         return try req.view().render("help")
     }
-    
+
     router.get("about") { req -> Future<View> in
         return try req.view().render("about")
     }
-    
+
     router.get("explore") { req -> Future<View> in
         return try req.view().render("explore")
     }
-    
+
+    //http://localhost:8080/staticjson
+    router.get("staticjson") { req -> String in
+      let text = ["headline":"Nr Processo: 007969031", "text":""]
+      var start_date : [String: Any] = ["year": 1900, "month": 1, "day": 1]
+      var moretext : [String: Any] = ["headline":"3358254","text":""]
+      var media = ["url" : "https://digital.lib.calpoly.edu/islandora/rest/v1/object/rekl:3037/datastream/TN"]
+      var events : [String: Any] = ["start_date": start_date, "text": moretext, "media" : media]
+      var eventarray = [events, events, events]
+      var jsonDic : [String: Any] = ["title": text, "events": eventarray]
+      let data = try JSONSerialization.data(withJSONObject: jsonDic, options: .prettyPrinted)
+      let jsonString = String(data: data, encoding: .utf8)
+      return jsonString ?? "FAILED"
+    }
+
+    //Example url: http://localhost:8080/dynamicjson?rekl=28694,8172,13660&cpsca=132,64,84&islandora=1087,1093,1282,983
+    //Should return as String like staticjson, but I couldn't figure it output
+    //
+    router.get("dynamicjson") { req -> Future<View> in
+      let client = try req.client()
+      let reklQuery = req.query[String.self, at: "rekl"] ?? ""
+      let reklArray = reklQuery.components(separatedBy: ",")
+      let islandoraQuery = req.query[String.self, at: "islandora"] ?? ""
+      let islandoraArray = islandoraQuery.components(separatedBy: ",")
+      let cpscaQuery = req.query[String.self, at: "cpsca"] ?? ""
+      let cpscaArray = cpscaQuery.components(separatedBy: ",")
+      var queryingURL = "https://digital.lib.calpoly.edu/islandora/rest/v1/solr/"
+      for identifier in reklArray{
+        queryingURL.append("PID:\"rekl:" + identifier + "\"")
+        queryingURL.append(" OR ")
+      }
+      for identifier in islandoraArray{
+        queryingURL.append("PID:\"islandora:" + identifier + "\"")
+        queryingURL.append(" OR ")
+      }
+      for identifier in cpscaArray{
+        queryingURL.append("PID:\"cpsca:" + identifier + "\"")
+        queryingURL.append(" OR ")
+      }
+      var truncated = queryingURL.substring(to: queryingURL.length - 4)
+      truncated.append("?rows=15&omitHeader=true&wt=json&start=0")
+      guard let encodedSearchTerm = truncated.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+          throw Abort(.badRequest)
+      }
+      return client.get(encodedSearchTerm, headers: HTTPHeaders.init([("User-Agent", "MorganApp/0.1"), ("Authorization", "Basic Y2FzdGxlX2NvbXB1dGluZzo4PnoqPUw0QmU2TWlEP1FB")]))
+      .flatMap { response -> Future<SearchResult> in
+          //Decode response into a SearchResult
+          try response.content.decode(SearchResult.self)
+      }
+      .flatMap { result-> Future<View>  in
+          var eventarray: [[String:Any]] = []
+          //Go through each entry and add it to json output
+          for entry in result.response.docs{
+            let date = entry.date ?? ""
+            var datearray = date.components(separatedBy: "-")
+            let description = entry.description ?? ""
+            let title = entry.title ?? ""
+            if date == ""{
+              datearray.append("")
+              datearray.append("")
+            }
+            var start_date : [String: Any] = ["year": datearray[0] ?? "", "month": datearray[1] ?? "", "day": datearray[2] ?? ""]
+            var titleanddescription : [String: Any] = ["headline":title,"text":description ?? ""]
+            var media = ["url" : "https://digital.lib.calpoly.edu/islandora/rest/v1/object/" + entry.pid + "/datastream/TN"]
+            var events : [String: Any] = ["start_date": start_date, "text": titleanddescription, "media" : media]
+            eventarray.append(events)
+          }
+          let text = ["headline":"Your Timeline", "text":"A Custom Morgan Letter Timeline"]
+          var jsonDic : [String: Any] = ["title": text, "events": eventarray]
+          let data = try JSONSerialization.data(withJSONObject: jsonDic, options: .prettyPrinted)
+          var jsonString = String(data: data, encoding: String.Encoding.ascii)
+          return try req.view().render("json", ResultPage(searchTerm: jsonString ?? "", searchResults: result.response.docs, numResults: result.response.numFound, start: result.response.start, page: 8))
+      }
+    }
+
+    //Either need to consolidate with dynamicjson or pass parameters to html and
+    //call dynamicjson from there
+    router.get("explorer") { req -> Future<View> in
+      return try req.view().render("explorer")
+    }
+
+=======
+    router.get("explore"){ req in
+        return try req.view().render("explore")
+    }
+>>>>>>> Stashed changes
     //Adds new route, with search parameter.
     //Returns a view in the future
     router.get("search") { req -> Future<View> in
@@ -53,29 +140,34 @@ public func routes(_ router: Router) throws {
         guard let searchTerm = req.query[String.self, at: "query"] else {
             throw Abort(.badRequest)
         }
-        
+
         var page = req.query[Int.self, at: "page"] ?? 1
         if page < 1 {
             page = 1
         }
-        
+
         let start = (page - 1) * 15
-        
+
         //Replaces all characters of searchTerm not in allowed characters with percent
         //Encoded characters. .urlQueryAllowed is the character set.
         guard let encodedSearchTerm = searchTerm.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
             throw Abort(.badRequest)
         }
-        
+
         //Create URL for Islandora entries with encoded search title and description
         //Using SOLR search parameters
         //Returns maximum 10 documents (rows=10)
         //Excludes header
+<<<<<<< Updated upstream
         let searchURL = searchBookURL(encodedSearchTerm: encodedSearchTerm, start: start)
-        
+        print(searchURL)
+=======
+        let searchURL = "https://digital.lib.calpoly.edu/islandora/rest/v1/solr/RELS_EXT_hasModel_uri_t:bookCModel%20AND%20ancestors_ms:%22rekl:morgan-ms010%22%20AND%20(dc.title:" + encodedSearchTerm + "%20OR%20dc.description:" + encodedSearchTerm + ")?rows=15&omitHeader=true&wt=json&start=" + String(start)
+
+>>>>>>> Stashed changes
         //Create a client to send a request.get()
         let client = try req.client()
-        
+
         //Sends an HTTP GET request to URL
         //The headers are required in the HTTP request, parameter User-Agent has value
         //MorganApp/0.1
