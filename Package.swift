86e37a245b1fc53c561c28a743891c74f5049740
Ethan Kusters <ethan@outlook.com>
2020-07-03 10:33:56 -0700

    Disabled SSL peer verificatin
    Installed CurlyClient and disabled ssl peer verification for all requests

--- Package.swift
+++ Package.swift
@@ -9,10 +9,11 @@ let package = Package(
 
         // 🍃 An expressive, performant, and extensible templating language built for Swift.
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
-        .package(url: "https://github.com/nodes-vapor/bootstrap.git", from: "1.0.0")
+        .package(url: "https://github.com/nodes-vapor/bootstrap.git", from: "1.0.0"),
+        .package(url: "https://github.com/vzsg/Curly.git", from: "0.6.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Leaf", "Vapor", "Bootstrap"]),
+        .target(name: "App", dependencies: ["Leaf", "Vapor", "Bootstrap", "CurlyClient"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,11 +1,15 @@
 import Leaf
 import Vapor
 import Bootstrap
+import CurlyClient
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     /// Register providers first
-    try services.register(LeafProvider())   
+    try services.register(LeafProvider())
+    try services.register(CurlyProvider(globalOptions: [
+        .sslVerifyPeer(false)
+    ]))
     
     /// Register routes to the router
     let router = EngineRouter.default()
@@ -14,7 +18,10 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
     /// Use Leaf for rendering views
     config.prefer(LeafRenderer.self, for: ViewRenderer.self)
-
+    
+    // Use Curly for client requests
+    config.prefer(CurlyClient.self, for: Client.self)
+    
     /// Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
